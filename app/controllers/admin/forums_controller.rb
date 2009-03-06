5d569449d6cc360d6d6c6a2ce655208b1968861e
will <will@spanner.org>
2009-03-06 08:58:26 +0000

    hiding actions from non-admin

--- app/controllers/admin/forums_controller.rb
+++ app/controllers/admin/forums_controller.rb
@@ -5,7 +5,7 @@ class Admin::ForumsController < Admin::ResourceController
 
   only_allow_access_to :new, :create, :edit, :update, :remove, :destroy,
     :when => :admin,
-    :denied_url => { :controller => 'pages', :action => 'index' },
+    :denied_url => { :controller => 'forum', :action => 'index' },
     :denied_message => 'You must be an administrator to add or change discussion categories.'
   
 end
