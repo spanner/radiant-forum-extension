436cad07cb1ae3c8a43fcdb309b749b11b4c7268
will <will@spanner.org>
2009-04-02 13:55:25 +0100

    users to readers

---
+++ db/migrate/007_reader_ownership.rb
@@ -0,0 +1,20 @@
+class ReaderOwnership < ActiveRecord::Migration
+  
+  # mostly for migrating old sites where users were talking
+  # but also useful for admin moderation, probably
+  # they already have created_at and updated_at
+  
+  def self.up
+    Post.find(:all).each do |post|
+      if Post.column_names.include?("user_id")
+        post.created_by = User.find(post.user_id) rescue nil
+      end
+      post.reader ||= Reader.find_or_create_for_user(post.created_by)
+      post.save
+    end
+  end
+
+  def self.down
+    
+  end
+end
