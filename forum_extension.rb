692c6996c7936e9e2b25150313055bbd7936d362
spanner <will@spanner.org>
2010-10-19 16:06:28 +0100

    small updates to keep up with reader: much still to do

--- forum_extension.rb
+++ forum_extension.rb
@@ -14,13 +14,13 @@ class ForumExtension < Radiant::Extension
     UserActionObserver.instance.send :add_observer!, Topic
     UserActionObserver.instance.send :add_observer!, Post
     Page.send :include, ForumTags
-    Admin::ReaderSettingsController.make_settable 'forum.editable_period' => 15, 'forum.public?' => true, 'forum.layout' => '', 'forum.allow_page_comments?' => true
     
     unless defined? admin.forum # UI is a singleton
       Radiant::AdminUI.send :include, ForumAdminUI
       admin.forum = Radiant::AdminUI.load_default_forum_regions
-      admin.pages.edit.add :parts_bottom, "edit_commentability", :after => "edit_layout_and_type"
-      admin.reader_settings.index.add :settings, "forum", :after => "sender"
+      # admin.pages.edit.add :parts_bottom, "edit_commentability", :after => "edit_layout_and_type"
+      admin.reader_configuration.show.add :settings, "forum", :after => "sender"
+      admin.reader_configuration.edit.add :form, "edit_forum", :after => "edit_sender"
       if defined? Site && admin.sites
         Site.send :include, ForumSite
       end
