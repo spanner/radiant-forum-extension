8e4ab3cd39b8a9a51a4e24bc2543ce675a0b54dc
spanner <will@spanner.org>
2011-06-08 16:08:55 +0100

    js update and some missed translations

--- public/javascripts/forum.js
+++ public/javascripts/forum.js
@@ -10,20 +10,22 @@
 
 (function($) { 
 
+  $.ajaxSettings.accepts.html = $.ajaxSettings.accepts.script;
+
   function RemoteAction (url, holder) {
     var self = this;
-		$.extend(self, {
-		  url: url,
-		  holder: holder,
-		  container: null,
-		  form: null,
+    $.extend(self, {
+      url: url,
+      holder: holder,
+      container: null,
+      form: null,
 
-		  fetch: function () {
-		    if (self.showing()) self.hide();
+      fetch: function () {
+        if (self.showing()) self.hide();
         else if (self.form) self.show();
         else {
           self.wait();
-          $.get(self.url, self.step);  
+          $.get(self.url, self.step, 'html');  
         }
       },
       submit: function (e) {
@@ -35,7 +37,7 @@
         if (ajaxable) {
           e.preventDefault();
           self.form.find('textarea.toolbarred').read_editor();
-          $.post(self.form.attr('action'), self.form.serialize(), self.step);  
+          $.post(self.form.attr('action'), self.form.serialize(), self.step, 'html');  
         } else {
           return true;  // allow event through so that uploads are sent by normal HTTP POST
                         // toolbar is read in onSubmit
@@ -56,7 +58,7 @@
         }
       },
       cancel: function (event) {
-        squash(event);
+        if (event) event.preventDefault();
         self.unwait();
         self.hide();
       },
@@ -78,31 +80,31 @@
       unwait: function () {
         holder.unwait();
       }
-		});
-		self.container = $('<div class="remote_form" />').hide();
-		self.holder.append(self.container);
+    });
+    self.container = $('<div class="remote_form" />').hide();
+    self.holder.append(self.container);
   }
   
-	function ActionHolder(container, conf) {   
+  function ActionHolder(container, conf) {   
     var self = this;
-		$.extend(self, {
-		  container: container,
-		  wrapper: container.find('.wrapper'),
-		  actions: {},
-		  initActions: function () {
-		    self.actions = {};
-    		self.container.find('a.remote').each(function () {
-    		  var a = $(this);
-    		  var href = a.attr('href');
+    $.extend(self, {
+      container: container,
+      wrapper: container.find('.wrapper'),
+      actions: {},
+      initActions: function () {
+        self.actions = {};
+        self.container.find('a.remote').each(function () {
+          var a = $(this);
+          var href = a.attr('href');
           self.addAction(href);
           a.click(function (event) {
-            squash(event);
+            if (event) event.preventDefault();
             a.addClass('waiting');
             self.showAction(href);
           });
           if (a.is('.autoload')) self.showAction(href);
-    		});
-		  },
+        });
+      },
       addAction: function (url) {
         if (!self.actions[url]) self.actions[url] = new RemoteAction(url, self);
         return self.actions[url];
@@ -127,7 +129,7 @@
         self.wrapper.hide();
       },
       toggle: function (event) {
-        squash(event);
+        if (event) event.preventDefault();
         if (self.wrapper.is(":visible")) self.hide();
         else self.show();
       },
@@ -139,23 +141,23 @@
         self.container.removeClass('waiting');
         self.container.find('a.remote').removeClass('waiting');
       }
-		});
-		self.initActions();
-	}
-	
-	$.fn.enable_remote_actions = function(conf) {
-		this.each(function() {			
-			new ActionHolder($(this), conf);
-		});
-		return this;
-	};
+    });
+    self.initActions();
+  }
+  
+  $.fn.enable_remote_actions = function(conf) {
+    this.each(function() {      
+      new ActionHolder($(this), conf);
+    });
+    return this;
+  };
 
-	function HideablePost(container, conf) {   
+  function HideablePost(container, conf) {   
     var self = this;
-		$.extend(self, {
-		  head: container.find('.post_header'),
-		  body: container.find('.post_body'),
-		  shower: null,
+    $.extend(self, {
+      head: container.find('.post_header'),
+      body: container.find('.post_body'),
+      shower: null,
       show: function () {
         self.body.slideDown();
         self.shower.text('Hide');
@@ -165,36 +167,36 @@
         self.shower.text('Show');
       },
       toggle: function (event) {
-        squash(event);
+        if (event) event.preventDefault();
         if (self.body.is(":visible")) self.hide();
         else self.show();
       }
-		});
+    });
     if ($('a.prev_page').length > 0) {
-  	  self.shower = $('<a href="#" class="shower">Hide</a>').appendTo(self.head.find('p.context'));
+      self.shower = $('<a href="#" class="shower">Hide</a>').appendTo(self.head.find('p.context'));
       self.shower.click(self.toggle);
       self.hide();
     }
-	}
+  }
 
-	$.fn.hideable_post = function() { 
-		this.each(function() {			
-			new HideablePost($(this));
-		});
-		return this;
-	};
+  $.fn.hideable_post = function() { 
+    this.each(function() {      
+      new HideablePost($(this));
+    });
+    return this;
+  };
 
-	function UploadStack(container) {   
+  function UploadStack(container) {   
     var self = this;
-		$.extend(self, {
-		  container: container,
+    $.extend(self, {
+      container: container,
       attachments_list: container.find('ul.attachments'),
       uploads_list: container.find('ul.uploads'),
       selector: container.find('div.selector'),
       file_field: container.find('div.selector').find('input'),
       
-			addUpload: function(event) {
-        squash(event);
+      addUpload: function(event) {
+        if (event) event.preventDefault();
         var upload_field = self.file_field.clone();
         var nest_id = self.attachmentCount() + self.uploadCount();  // nb. starts at zero so this total is +1
         var container = $('<li class="attachment">' + upload_field.val() + '</li>');
@@ -225,56 +227,56 @@
     });
     self.attachments_list.find('li').add_remover();
     self.file_field.change(self.addUpload);
-	}
-	
-	$.fn.upload_stack = function(conf) { 
-		this.each(function() {			
-			el = new UploadStack($(this), conf);
-		});
-		return this;
-	};
+  }
+  
+  $.fn.upload_stack = function(conf) { 
+    this.each(function() {      
+      el = new UploadStack($(this), conf);
+    });
+    return this;
+  };
 
-	$.fn.add_remover = function() { 
-	  this.each(function() {
-  	  var self = $(this);
+  $.fn.add_remover = function() { 
+    this.each(function() {
+      var self = $(this);
       var remover = $('<a href="#" class="remove">remove</a>');
       remover.click(function (event) { 
-        squash(event);
+        if (event) event.preventDefault();
         self.slideUp('500', function() { 
           self.find('input.checkbox').attr('checked', true);
           self.find('input.filefield').remove();
         }); 
       });
       self.append(remover);
-	  });
-		return self;
-	};
+    });
+    return self;
+  };
 
-	$.fn.add_editor = function() { 
-		this.each(function() { 
-		  var self = $(this);
+  $.fn.add_editor = function() { 
+    this.each(function() { 
+      var self = $(this);
       var editor = new punymce.Editor({
         id : self.attr('id'),
         plugins : 'Link,Image,Emoticons,EditSource',
         toolbar : 'bold,italic,link,unlink,image,emoticons,editsource',
         width : 510,
-  			height : 375,
+        height : 375,
         resize : true
       });
       self.data('editor', editor);
-		});
-		return this;
-	};
+    });
+    return this;
+  };
 
-	$.fn.read_editor = function() { 
-		this.each(function() { 
-		  var self = $(this);
-		  if (self.data('editor')) {
-		    self.val(self.data('editor').getContent());
-		  }
-		});
-		return this;
-	};
+  $.fn.read_editor = function() { 
+    this.each(function() { 
+      var self = $(this);
+      if (self.data('editor')) {
+        self.val(self.data('editor').getContent());
+      }
+    });
+    return this;
+  };
 
 })(jQuery);
 
@@ -285,7 +287,7 @@ $(function() {
   $(".upload_stack").upload_stack({});
   $(".toolbarred").add_editor({});
   $("input:submit").live('click', function (event) {
-	  var self = $(this);
+    var self = $(this);
     self.after('<span class="waiting">Please wait</span>');
     self.hide();
     return true;
