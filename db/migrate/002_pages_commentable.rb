2b995e68a49adb731f0de05fa815b57478de755e
spanner <will@spanner.org>
2011-06-13 17:04:16 +0100

    update to old migration in the hope that it prevents blockage. closes #9

--- db/migrate/002_pages_commentable.rb
+++ db/migrate/002_pages_commentable.rb
@@ -3,8 +3,7 @@ class PagesCommentable < ActiveRecord::Migration
     add_column :pages, :commentable, :boolean, :default => true
     add_column :pages, :comments_closed, :boolean, :default => false
     add_column :topics, :page_id, :integer
-    add_index :topics, :page_id
-    
+    add_index :topics, :page_id, :name => "index_topics_on_page_id"
   end
 
   def self.down
