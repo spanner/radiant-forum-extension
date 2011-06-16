bda2d39a9fc799d760c03a6d5166448904ebbb2c
spanner <will@spanner.org>
2011-06-16 17:33:42 +0100

    reader has no topics! closes #20

--- lib/forum_reader.rb
+++ lib/forum_reader.rb
@@ -2,7 +2,6 @@ module ForumReader
 
   def self.included(base)
     base.class_eval {
-      has_many :topics, :dependent => :nullify
       has_many :posts, :order => 'posts.created_at desc', :dependent => :nullify
       
       named_scope :most_commenting, lambda { |count|
