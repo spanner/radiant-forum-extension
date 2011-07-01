8c6d07d7c430008cf04d5fd923c75abbe0c2499e
spanner <will@spanner.org>
2011-07-01 16:13:44 +0100

    still_commentable? checks against published_at rather than created_at. closes #40. Thanks.

--- lib/forum_page.rb
+++ lib/forum_page.rb
@@ -19,7 +19,7 @@ module ForumPage
       return false unless Radiant::Config['forum.allow_page_comments?'] && commentable?
       return false if comments_closed?
       return true unless commentable_period && commentable_period > 0
-      return Time.now - self.created_at < commentable_period
+      return Time.now - self.published_at < commentable_period
     end
     
     def locked?
