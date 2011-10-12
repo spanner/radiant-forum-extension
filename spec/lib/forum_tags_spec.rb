45993c226393d6f33bf3db07aae341fe11b072f6
William Ross <will@spanner.org>
2011-10-12 10:28:35 +0100

    forum-group access uses standard control. tests updated

--- spec/lib/forum_tags_spec.rb
+++ spec/lib/forum_tags_spec.rb
@@ -4,24 +4,24 @@ describe "Forum Tags" do
   dataset :forums
 
   let(:page){ pages(:commentable) }
+  let(:reader){ readers(:normal) }
 
   describe "r:forum:topic tags" do
     let(:topic){ topics(:older) }
     let(:sticky){ topics(:sticky) }
     let(:on_date){ I18n.l(topic.created_at, :format => :standard) }
     let(:sticky_date){ I18n.l(sticky.created_at, :format => :standard) }
-    
     subject { page }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:name /></r:forum:topic>}).as(topic.name) }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:body /></r:forum:topic>}).as(topic.posts.first.body_html) }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:author /></r:forum:topic>}).as("Normal") }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:date /></r:forum:topic>}).as(on_date) }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:url /></r:forum:topic>}).as("/forum/forums/#{topic.forum.id}/topics/#{topic.id}") }
-    it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:context /></r:forum:topic>}).as(%{reply from <a href="#{reader_url(reader_id(:normal))}">Normal</a>}) }
+    it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:context /></r:forum:topic>}).as(%{reply from <a href="/directory/readers/#{reader.id}">Normal</a>}) }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:body /></r:forum:topic>}).as("<p>original topic message</p>") }
     it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:link /></r:forum:topic>}).as(%{<a href="/forum/forums/#{topic.forum.id}/topics/#{topic.id}">#{topic.name}</a>}) }
-    it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:summary /></r:forum:topic>}).as(%{<li><a href="/forum/forums/#{topic.forum.id}/topics/#{topic.id}">#{topic.name}</a><br />reply from <a href=#{reader_url(reader_id(:normal))}">Normal</a></li>}) }
-    it { should render(%{<r:forum:topic id="#{sticky.id}"><r:forum:topic:summary /></r:forum:topic>}).as(%{<li><a href="/forum/forums/#{sticky.forum.id}/topics/#{sticky.id}">#{sticky.name}</a><br />Started by <a href="#{reader_url(reader_id(:normal))}">Normal</a></li>}) }
+    it { should render(%{<r:forum:topic id="#{topic.id}"><r:forum:topic:summary /></r:forum:topic>}).as(%{<li><a href="/forum/forums/#{topic.forum.id}/topics/#{topic.id}">#{topic.name}</a><br />reply from <a href="/directory/readers/#{reader.id}">Normal</a></li>}) }
+    it { should render(%{<r:forum:topic id="#{sticky.id}"><r:forum:topic:summary /></r:forum:topic>}).as(%{<li><a href="/forum/forums/#{sticky.forum.id}/topics/#{sticky.id}">#{sticky.name}</a><br />Started by <a href="/directory/readers/#{reader.id}">Normal</a></li>}) }
   end
 
   describe "r:forum:post tags" do
